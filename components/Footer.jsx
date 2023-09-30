3ebf0633ee373664f9a87f8492b57c905b993d08
kirti <shuklakirti2004@gmail.com>
2023-09-30 16:37:43 +0530

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,26 +2,41 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gradient-to-b from-purple-400  to-red-270 text-white  p-10  mt-10 pb-36">
+    <footer className="bg-gradient-to-b from-purple-400 to-black text-white p-10 mt-10 pb-36 text-lg">
       <div className="container mx-auto">
-        <div className="text-left">
-          <p>&copy; 2023 by Technorate Animations</p>
-          <p className="text-right">
-            <a href="">
+        <div className="flex justify-between items-center">
+          <div>
+            <p>&copy; 2023 by Technorate Animations</p>
+          </div>
+          <div>
+            <a
+              href="#"
+              className="hover:text-purple-600 transition-colors duration-300"
+            >
               <img
                 src="icons8-twitter-48.png"
-                className="h-10 w-10"
-                alt="image"
+                className="h-10 w-10 inline-block mr-4"
+                alt="Twitter"
               />
             </a>
-          </p>
-          <p>
-            <a href="">
-              <img src="icons8-facebook-48.png" className="h-10 w-10" alt="" />
+            <a
+              href="#"
+              className="hover:text-purple-600 transition-colors duration-300"
+            >
+              <img
+                src="icons8-facebook-48.png"
+                className="h-10 w-10 inline-block"
+                alt="Facebook"
+              />
             </a>
+          </div>
+        </div>
+        <div className="flex  ">
+          <p className="mt-3">&#128241; +91 - 8251936822</p>
+
+          <p className="mt-3 ml-10 ">
+            &#128231; technorateanimations@gmail.com
           </p>
-          <p>&#128241; +91 - 8251936822</p>
-          <p>&#128231;technorateanimations@gmail.com</p>
         </div>
       </div>
     </footer>
