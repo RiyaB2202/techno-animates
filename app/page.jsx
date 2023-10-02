5ca58f91146f72d3e8873f5108c1f450dd40ac30
RiyaB2202 <riyabhardwaj470@gmail.com>
2023-10-02 15:49:34 +0530

    footer updates

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,32 @@
 "use client";
-import {
-  CloudArrowUpIcon,
-  LockClosedIcon,
-  ServerIcon,
-} from "@heroicons/react/20/solid";
+import Header from "@/component/Header";
+// import {
+//   CloudArrowUpIcon,
+//   LockClosedIcon,
+//   ServerIcon,
+// } from "@heroicons/react/20/solid";
 import { useTypewriter } from "react-simple-typewriter";
 
-const features = [
-  {
-    name: "Push to deploy.",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: "SSL certificates.",
-    description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
-    icon: LockClosedIcon,
-  },
-  {
-    name: "Database backups.",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
-    icon: ServerIcon,
-  },
-];
+// const features = [
+//   {
+//     name: "Push to deploy.",
+//     description:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+//     icon: CloudArrowUpIcon,
+//   },
+//   {
+//     name: "SSL certificates.",
+//     description:
+//       "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+//     icon: LockClosedIcon,
+//   },
+//   {
+//     name: "Database backups.",
+//     description:
+//       "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+//     icon: ServerIcon,
+//   },
+// ];
 
 export default function Home() {
   const [text] = useTypewriter({
--- component/Header.jsx
+++ component/Header.jsx
@@ -2,28 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className="header">
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li class="logo"><img src="/logo-techno.png" alt="Techbo Logo" /></li>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Services</a>
-        </li>
-        <li>
-          <a href="/contact">Samples</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+    
+    <nav className="bg-black text-white flex justify-between">
+    <img src="tra logo.jpeg" class="h-11 pt-1 px-3" alt="logo" srcset=""></img>
+    <ul className= " px-28 py-4 flex space-x-11 justify-end ">
+<li><i className="fa-solid fa-house" style="color: #33dee1;"></i>Home</li>
+<li>About</li>
+<li>Services</li>
+<li>Samples</li>
+<li>Contact</li>
+
+    </ul>
+</nav>
+
   )
 }
 
